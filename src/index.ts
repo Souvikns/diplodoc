263a2f1c4567527444b20435c345bab4cabab12c
Souvik <souvikde.ns@gmail.com>
2021-03-18 22:34:41 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-import path from 'path';
+import { Command } from 'commander';
+import chalk from 'chalk';
 
-console.log(path.resolve(__dirname));
+const program = new Command();
+
+program
+    .command('create <name>')
+    .action((name) => {
+        console.log("cd",chalk.blue(`${name}`));
+    })
+
+
+program.parse(process.argv);
