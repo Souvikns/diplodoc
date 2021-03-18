1ef14c96b34e4ef6f2c609733eb64c1568f63078
Souvik <souvikde.ns@gmail.com>
2021-03-18 23:12:33 +0530

    converted to js

---
+++ bin/diplodoc.js
@@ -0,0 +1,13 @@
+const { Command } = require('commander');
+const chalk = require('chalk');
+
+const program = new Command();
+
+program
+    .command("create <name>")
+    .action((name) => {
+        console.log(`cd ${chalk.bgRed(name)}`);
+    })
+
+
+program.parse(process.argv);
