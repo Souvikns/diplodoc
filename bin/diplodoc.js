481e35b9e482aec8e2f2897b37cfe4d41e0c6064
Souvik <souvikde.ns@gmail.com>
2021-03-21 14:37:49 +0530

    Update diplodoc.js

--- bin/diplodoc.js
+++ bin/diplodoc.js
@@ -2,15 +2,12 @@
 
 const { Command } = require('commander');
 const chalk = require('chalk');
-const {generateProject} = require('../lib/create');
 
 const program = new Command();
 
 program
     .command("create <name>")
     .action((name) => {
-        generateProject({projectName: name});
-        console.log(chalk.redBright(`cd ${name}`));
     })
 
 program
