4344405cb4a0a8c1fb3f4c39a9d8be541416381d
Souvik De <souvikde.ns@gmail.com>
2021-04-11 12:55:37 +0530

    Update util.js

--- bin/util.js
+++ bin/util.js
@@ -1,27 +1,28 @@
 const inquirer = require('inquirer');
-
-exports.getCreateQuestions = () => {
-    return inquirer.prompt([
-        {
-            name: 'license',
-            message: "Choose a license",
-            type: "list",
-            choices: [
-                "afl-3.0",
-                "apache-2.0",
-                "gpl",
-                "mit",
-                "isc"
-            ]
-        },
-        {
-            name: 'pkgManager',
-            message: "Choose a Package Manager",
-            type: "list",
-            choices: [
-                "npm",
-                "yarn"
-            ]
-        }
-    ]);
+module.exports = {
+    getCreateQuestions: () => {
+        return inquirer.prompt([
+            {
+                name: 'license',
+                message: "Choose a license",
+                type: "list",
+                choices: [
+                    "afl-3.0",
+                    "apache-2.0",
+                    "gpl",
+                    "mit",
+                    "isc"
+                ]
+            },
+            {
+                name: 'pkgManager',
+                message: "Choose a Package Manager",
+                type: "list",
+                choices: [
+                    "npm",
+                    "yarn"
+                ]
+            }
+        ]);
+    }
 }
