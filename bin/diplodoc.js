928ae473b6de18bc192a0127c6b9c5a1ea2296bd
Restyled.io <commits@restyled.io>
2021-04-23 23:16:32 +0000

    Restyled by clang-format

--- bin/diplodoc.js
+++ bin/diplodoc.js
@@ -3,42 +3,35 @@
 /**
  * diplodoc <action/verb> <resource>
  */
-const { Command } = require('commander');
+const {Command} = require('commander');
 const chalk = require('chalk');
 const package = require('../package.json');
 
 const program = new Command();
-program
-    .name("diplodoc")
-    .addHelpCommand(false)
-    .version(package.version);
+program.name("diplodoc").addHelpCommand(false).version(package.version);
 
-program
-    .command("create")
+program.command("create")
     .description("Create project")
     .option('-t, --template <templateName>', "add template configuration")
-    .action(async (options) => {
-        console.log(options);
-    })
+    .action(async (options) => { console.log(options); })
 
-program
-    .command("build")
+program.command("build")
     .option('-c, --config <configPath>', "relative path of the config file")
     .description("create build")
     .action(() => {
 
-    })
+            })
 
 program.command("dev")
     .description("run development server")
     .option('-c, --config <configPath>', "relative path of the config file")
     .action(() => {
 
-    })
+            })
 
 program.on('command:*', (command) => {
-    console.error(chalk.redBright(`error: invalid command ${command}`));
-    program.help();
+  console.error(chalk.redBright(`error: invalid command ${command}`));
+  program.help();
 })
 
 program.parse(process.argv);
