3417158f3d4901a17f486555bb2f22fea589bf43
Restyled.io <commits@restyled.io>
2021-04-23 19:18:28 +0000

    Restyled by clang-format

--- bin/util.js
+++ bin/util.js
@@ -3,51 +3,42 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('lodash')
 module.exports = {
-    getCreateQuestions: () => {
-        return inquirer.prompt([
-            {
-                name: 'license',
-                message: "Choose a license",
-                type: "list",
-                choices: [
-                    "afl-3.0",
-                    "apache-2.0",
-                    "gpl",
-                    "mit",
-                    "isc"
-                ]
-            },
-            {
-                name: 'pkgManager',
-                message: "Choose a Package Manager",
-                type: "list",
-                choices: [
-                    "npm",
-                    "yarn"
-                ]
-            }
-        ]);
-    },
-    /**
-     * 
-     * @param {string} appDir - path of the working directory
-     * @param {string} fileName - name of the config file 
-     * @returns {object} config object
-     */
-    LoadConfigFile: (appDir, fileName) => {
-        let filePath;
+  getCreateQuestions : () => {
+    return inquirer.prompt([
+      {
+        name : 'license',
+        message : "Choose a license",
+        type : "list",
+        choices : [ "afl-3.0", "apache-2.0", "gpl", "mit", "isc" ]
+      },
+      {
+        name : 'pkgManager',
+        message : "Choose a Package Manager",
+        type : "list",
+        choices : [ "npm", "yarn" ]
+      }
+    ]);
+  },
+  /**
+   *
+   * @param {string} appDir - path of the working directory
+   * @param {string} fileName - name of the config file
+   * @returns {object} config object
+   */
+  LoadConfigFile : (appDir, fileName) => {
+    let filePath;
 
-        if (_.isUndefined(fileName)) {
-            filePath = path.resolve(appDir, "diplodoc.config.js");
-        } else {
-            filePath = path.resolve(appDir, fileName);
-        }
-
-        let config = require(filePath);
-
-        if (!_.isObject(config)) throw new Error("Something wrong with the config File");
-
-        return config;
-
+    if (_.isUndefined(fileName)) {
+      filePath = path.resolve(appDir, "diplodoc.config.js");
+    } else {
+      filePath = path.resolve(appDir, fileName);
     }
+
+    let config = require(filePath);
+
+    if (!_.isObject(config))
+      throw new Error("Something wrong with the config File");
+
+    return config;
+  }
 }
